72428c0e7fcf44afc192982be70ff6e60e8289c0
davidtruong67 <truongdinhhuy96@gmail.com>
2025-06-14 23:10:10 +0700

    Update tidb_api.py

--- agent/tidb_api.py
+++ agent/tidb_api.py
@@ -1,1 +1,13 @@
+import requests
 
+API_URL = "https://ap-southeast-1.data.tidbcloud.com/api/v1beta/app/dataapp-EpbxftUE/endpoint"
+
+def query_tidb(sql: str):
+    try:
+        res = requests.post(API_URL, json={"sql": sql})
+        if res.status_code == 200:
+            return res.json()
+        else:
+            return f"Lỗi: {res.status_code} - {res.text}"
+    except Exception as e:
+        return f"Exception: {str(e)}"
